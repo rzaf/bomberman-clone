49b293945c1ecfc12030085a1836b20e02cd26d1
rzaf <rasfia79@gmail.com>
2023-12-18 19:32:39 +0330

    Add clock sprite to ui

--- states/running/ui.go
+++ states/running/ui.go
@@ -8,14 +8,16 @@ import (
 )
 
 var (
-	p1Texture *core.Texture
-	p2Texture *core.Texture
-	timeStr   string
+	p1Texture    *core.Texture
+	p2Texture    *core.Texture
+	clockTexture *core.Texture
+	timeStr      string
 )
 
 func loadUi() {
-	p1Texture = core.GetTexture("anims").Crop(ray.NewRectangle(64, 0, 17, 14))
-	p2Texture = core.GetTexture("anims").Crop(ray.NewRectangle(167, 0, 17, 14))
+	p1Texture = core.GetTexture("tiles").Crop(ray.NewRectangle(291, 0, 15, 14))
+	p2Texture = core.GetTexture("tiles").Crop(ray.NewRectangle(306, 0, 15, 14))
+	clockTexture = core.GetTexture("tiles").Crop(ray.NewRectangle(321, 0, 20, 20))
 }
 
 func updateUi() {
@@ -26,14 +28,14 @@ func updateUi() {
 }
 
 func drawUi() {
-	p1Texture.DrawAt(ray.NewRectangle(0, 20, 25, 30))
-	p2Texture.DrawAt(ray.NewRectangle(0, 70, 25, 30))
-
+	p1Texture.DrawAt(ray.NewRectangle(4, 20, 30, 28))
+	p2Texture.DrawAt(ray.NewRectangle(4, 70, 30, 28))
+	clockTexture.DrawAt(ray.NewRectangle(4, 110, 30, 30))
 	p1.Lock()
 	p2.Lock()
-	ray.DrawText(fmt.Sprintf("%d", p1.Wins), 28, 26, 25, ray.White)
-	ray.DrawText(fmt.Sprintf("%d", p2.Wins), 28, 76, 25, ray.Black)
+	ray.DrawText(fmt.Sprintf("%d", p1.Wins), 38, 26, 25, ray.White)
+	ray.DrawText(fmt.Sprintf("%d", p2.Wins), 38, 76, 25, ray.Black)
 	p1.Unlock()
 	p2.Unlock()
-	ray.DrawText(timeStr, 30, 115, 25, ray.White)
+	ray.DrawText(timeStr, 38, 115, 25, ray.White)
 }
